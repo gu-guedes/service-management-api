273e519b9d8fd699d7d32858a1606795f8f3a685
guguedes <gustavo.guedes@nepreconsulting.com>
2026-02-05 13:24:05 -0300

    feat: add springdoc-openapi dependency and configure Maven repository

--- src/main/java/com/example/service_management/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/service_management/exception/ResourceNotFoundException.java
@@ -2,15 +2,7 @@
 package com.example.service_management.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException() {
-        super();
-    }
-
     public ResourceNotFoundException(String message) {
         super(message);
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
